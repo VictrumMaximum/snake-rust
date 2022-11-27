2eeca30c8ffff4dcd58d922e06e0125f722f612d
Victor Ionescu <victor@meldeenvermoeden.nl>
2022-11-27 21:31:21 +0100

    init and restore alt screen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{stdout, Error, Write};
+
+use crossterm::{
+    cursor::{DisableBlinking, EnableBlinking},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+
+fn main() -> Result<(), Error> {
+    let out = stdout();
+
+    init_screen(&out)?;
+
+    restore_screen(&out)?;
+
+    Ok(())
+}
+
+fn init_screen(mut out: impl Write) -> Result<(), Error> {
+    enable_raw_mode()?;
+    execute!(out, EnterAlternateScreen, DisableBlinking)
+}
+
+fn restore_screen(mut out: impl Write) -> Result<(), Error> {
+    disable_raw_mode()?;
+    execute!(out, LeaveAlternateScreen, EnableBlinking)
 }
